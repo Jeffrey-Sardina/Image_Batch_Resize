5aba07139d1716b4e11fce38b379006f7c8aa395
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2019-10-25 21:28:20 -0700

    basic functionality

---
+++ batchResize.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+import sys
+import glob
+import os
+
+'''
+Make sure the image has the same pixel size as the given dimmensions
+
+References:
+    https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
+    https://stackoverflow.com/questions/33662693/add-pixels-in-python-opencv
+    http://tsaith.github.io/combine-images-into-a-video-with-python-3-and-opencv-3.html
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.concatenate.html
+    https://stackoverflow.com/questions/12881926/create-a-new-rgb-opencv-image-using-python#12890573
+'''
+def resize_img(img_frame, width, height):
+    #Get ratios
+    img_height, img_width, channels = img_frame.shape
+    scalar_difference_height = height / img_height
+    scalar_difference_width = width / img_width
+
+    #Resize image so that is is the same as the video aspect ratio in one dim, and less than it (or equal) in another
+    scalar = 0
+    if (img_height * scalar_difference_width) > height:
+        scalar = scalar_difference_height
+    else:
+        scalar = scalar_difference_width
+
+    new_width = int(img_width * scalar)
+    new_height = int(img_height * scalar)
+    new_size = (new_width, new_height)
+    resized = cv2.resize(img_frame, (new_width, new_height))
+
+    #If both dims are equal, we are done
+    if new_size == (width, height):
+        return resized
+
+    #If not, we need to add blank (black) pixels. Note that a frame is a numpy array of pixels with values 0 to 255
+    if new_width == width: #we need to add pixels to the top and bottom
+        difference = height - new_height
+        top_diff = difference // 2
+        bottom_diff = difference - top_diff
+
+        top = np.zeros((top_diff, width, channels), np.uint8)
+        bottom = np.zeros((bottom_diff, width, channels), np.uint8)
+
+        resized = np.concatenate((top, resized), axis = 0)
+        resized = np.concatenate((resized, bottom), axis = 0)
+    else: #we need to add pixels to the sides
+        difference = width - new_width
+        left_diff = difference // 2
+        right_diff = difference - left_diff
+
+        left = np.zeros((height, left_diff, channels), np.uint8)
+        right = np.zeros((height, right_diff, channels), np.uint8)
+
+        resized = np.concatenate((left, resized), axis = 1)
+        resized = np.concatenate((resized, right), axis = 1)
+
+    return resized
+
+'''
+Batch converts the images at a given file location to given dimensions and writes them out into given folder
+
+References:
+    https://docs.opencv.org/2.4/doc/tutorials/introduction/load_save_image/load_save_image.html
+'''
+def main():
+    #Handle incorrect input
+    if(len(sys.argv) < 4):
+        print('Please provide the input folder, outpug folder, desired width, and desired height')
+        print('For example: python batchResize.py in/folder/ out/folder/ 640 480')
+        exit()
+
+    #Collect input and handle invalid input
+    inDir = sys.argv[1]
+    outDir = sys.argv[2]
+    try:
+        width = int(sys.argv[3])
+        height = int(sys.argv[4])
+    except:
+        print('Invalid width or height value')
+        print('Please only enter positive integers for width and height')
+        print('For example: python batchResize.py in/folder/ out/folder/ 640 480')
+        exit()
+
+    #Get image names (base names only)
+    os.chdir(inDir)
+    image_names = glob.glob('*')
+    os.chdir('..')
+
+    #Resize images
+    for image_name in image_names:
+        img_frame = resize_img(cv2.imread(os.path.join(inDir, image_name)), width, height)
+        cv2.imwrite(os.path.join(outDir, image_name), img_frame)
+
+main()
